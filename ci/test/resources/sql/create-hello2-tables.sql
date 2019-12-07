6f51267352909a22baa0763143053a96abf1ca9a
ci <ci@hail.is>
2019-12-07 21:02:44 +0000

    setup repo

---
+++ ci/test/resources/sql/create-hello2-tables.sql
@@ -0,0 +1,4 @@
+
+CREATE TABLE `hello` (
+  greeting VARCHAR(100) NOT NULL
+) ENGINE = InnoDB;
---
+++ ci/test/resources/sql/insert.py
@@ -0,0 +1,16 @@
+import asyncio
+from gear import Database
+
+
+async def async_main():
+    db = Database()
+    await db.async_init()
+
+    await db.just_execute(f'''
+INSERT INTO hello (greeting)
+VALUES 'hello, hello!';
+''')
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(async_main())
